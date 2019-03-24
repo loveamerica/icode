9b8c921a8c4051f64d86cf9dea6c6eb05a4890f2
claire420 <claire_liu325@163.com>
2019-03-24 22:32:10 +0800

    two sums

---
+++ src/main/java/algorithm/twoSum/Solution.java
@@ -0,0 +1,25 @@
+package twoSum;
+
+public class Solution {
+
+	
+	 public int[] twoSum(int[] nums, int target) {
+	        
+	       int[] ret = new int[2];
+			
+			for(int i=0;i<nums.length;i++) {
+				
+				for(int j=i+1;j<nums.length;j++) {
+					
+					if(target == nums[i]+nums[j]) {
+						ret[0] = i;
+						ret[1] = j;
+						return ret;
+						
+					}
+				}
+				
+			}
+			return ret;    
+	    }
+}
